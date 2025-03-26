5251a52d92554908e9df6df8e002b03640ed055c
Eduardo Cordovil <86244886+EduardoSilvaCordovil@users.noreply.github.com>
2025-03-26 14:25:47 -0300

    app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function startPwa(firstStart) {
 // Configuração de Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
           .then(registration => {
               console.log('Service Worker registrado com sucesso:', registration);
           })
