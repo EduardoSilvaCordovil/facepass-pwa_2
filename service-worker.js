50d101b0624956fa47c2529bcfee300af4023cb4
Eduardo Cordovil <86244886+EduardoSilvaCordovil@users.noreply.github.com>
2025-03-26 14:39:14 -0300

    index modificado

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-//const CACHE_NAME = 'facepass-v1';
+const CACHE_NAME = "facepass-v1";
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open("pwa").then(function (cache) {
+    caches.open("facepass-v1").then(function (cache) {
       return cache.addAll([
         "/",
         "/style.css",
@@ -14,7 +14,7 @@ self.addEventListener("install", function (event) {
 
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.open("pwa").then(function (cache) {
+    caches.open("facepass-v1").then(function (cache) {
       return cache.match(event.request).then(function (response) {
         cache.addAll([event.request.url]);
 
